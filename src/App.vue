22be87d118f7966a16c8091fc8c9a5b9d42468ce
lDeityl <mihailov_221105@mail.ru>
2024-07-10 02:09:56 +0300

    template, exercise with options api and composition api

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import MyHello from './components/MyHello.vue'
+</script>
+
+<template>
+    <h1>Главная</h1>
+    <MyHello />
+</template>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const count = ref(0);
+
+const inc = () => count.value++;
+
+const dis = () => count.value--;
+
+</script>
+
+<template>
+    <h1>{{ count }}</h1>
+    <button @click="inc">+</button>
+    <button @click="dis">-</button>
+</template>
